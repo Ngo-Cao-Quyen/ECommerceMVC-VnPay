2d6093f51714be7d39b13c276e606f786fdc926f
ngocaoquyen <ngocaoquyen2000@gmail.com>
2024-05-23 15:33:32 +0700

    Add project files.

---
+++ ECommerceMVC/Program.cs
@@ -0,0 +1,63 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.Helpers;
+using ECommerceMVC.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<QShopContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("QShop"));
+});
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+	options.IdleTimeout = TimeSpan.FromMinutes(10);
+	options.Cookie.HttpOnly = true;
+	options.Cookie.IsEssential = true;
+});
+
+// https://docs.automapper.org/en/stable/Dependency-injection.html
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+// https://learn.microsoft.com/en-us/aspnet/core/security/authentication/cookie?view=aspnetcore-8.0
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+	options.LoginPath = "/KhachHang/DangNhap";
+	options.AccessDeniedPath = "/AccessDenied";
+});
+
+
+builder.Services.AddSingleton<IVnPayService, VnPayService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Resources/SQL/dbo.Loai.data.sql
@@ -0,0 +1,10 @@
+SET IDENTITY_INSERT [dbo].[Loai] ON
+INSERT INTO [dbo].[Loai] ([MaLoai], [TenLoai], [TenLoaiAlias], [MoTa], [Hinh]) VALUES (1000, N'Đồng hồ', N'dong-ho', N'Soft drinks, coffees, teas, beers, and ales', N'Best.png')
+INSERT INTO [dbo].[Loai] ([MaLoai], [TenLoai], [TenLoaiAlias], [MoTa], [Hinh]) VALUES (1001, N'Laptop', N'lap-top', N'Sweet and savory sauces, relishes, spreads, and seasonings', N'Certificate.png')
+INSERT INTO [dbo].[Loai] ([MaLoai], [TenLoai], [TenLoaiAlias], [MoTa], [Hinh]) VALUES (1002, N'Máy ảnh', N'may-anh', N'Desserts, candies, and sweet breads', N'')
+INSERT INTO [dbo].[Loai] ([MaLoai], [TenLoai], [TenLoaiAlias], [MoTa], [Hinh]) VALUES (1003, N'Điện thoại', N'dien-thoai', N'Cheeses', N'Bell.png')
+INSERT INTO [dbo].[Loai] ([MaLoai], [TenLoai], [TenLoaiAlias], [MoTa], [Hinh]) VALUES (1004, N'Nước hoa', N'nuoc-hoa', N'Breads, crackers, pasta, and cereal', N'Favourites.png')
+INSERT INTO [dbo].[Loai] ([MaLoai], [TenLoai], [TenLoaiAlias], [MoTa], [Hinh]) VALUES (1005, N'Trang sức', N'trang-suc', N'Prepared meats', N'Dice.png')
+INSERT INTO [dbo].[Loai] ([MaLoai], [TenLoai], [TenLoaiAlias], [MoTa], [Hinh]) VALUES (1006, N'Giày', N'giay', N'Dried fruit and bean curd', N'')
+INSERT INTO [dbo].[Loai] ([MaLoai], [TenLoai], [TenLoaiAlias], [MoTa], [Hinh]) VALUES (1007, N'Vali', N'vali', N'Seaweed and fish', N'Microsoft.png')
+SET IDENTITY_INSERT [dbo].[Loai] OFF
